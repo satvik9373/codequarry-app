28221bfdb4bfad6bb0529db433dd49d4322f09f4
satvik9373 <113889674+satvik9373@users.noreply.github.com>
2023-11-26 23:10:30 +0530

    aa

--- App/Screen/LoginScreen.js
+++ App/Screen/LoginScreen.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import Colors from '../Utils/Colors';
 import google from './../../assets/images/google.png';
-
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useWarmUpBrowser } from "../hooks/warmUpBrowser";
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
-  const onPress = () => {
-    
-  };
+  useWarmUpBrowser();
+ 
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+ 
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+  
 
   return (
     <View style={{ display: 'flex', alignItems: 'center' }}>
