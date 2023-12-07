4d0ab8c72b77c18ac6d5cd65e4d13495e76f978c
satvik9373 <113889674+satvik9373@users.noreply.github.com>
2023-12-07 22:18:18 +0530

    a

--- App/Components/ChapterContent/ProgressBar.js
+++ App/Components/ChapterContent/ProgressBar.js
@@ -2,16 +2,21 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import Colors from '../../Utils/Colors';
 
-export default function ProgressBar({ contentLength, contentIndex }) {
-  const arraySize =Array.from({ length: contentLength }, (_, index) => index + 1);
-  const width = 100 / contentLength;
+export default function ProgressBar({contentLength,contentIndex}) {
+  const arraySize=Array.from({length:contentLength},(_,index)=>index+1);
+  const width=100/contentLength;
 
   return (
-    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}> 
-      {arraySize.map((item, index) => (
-        <View key={index} style={{ backgroundColor: Colors.SECONDARY, width: width, borderRadius: 10, height: 10, margin: 5 }}>
-        </View>
+    <View style={{display:'flex',flexDirection:'row',
+    justifyContent:'space-between',marginTop:20,padding:20}}>
+      {arraySize.map((item,index)=>(
+        <View style={{
+          backgroundColor:`${index<=contentIndex?
+          Colors.GREEN:Colors.GRAY}`,
+      width:width+"%",borderRadius:10,height:10,margin:5,
+      flex:1}}>
+      </View>
       ))}
     </View>
-  );
+  )
 }
